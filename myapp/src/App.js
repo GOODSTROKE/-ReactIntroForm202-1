90aec83572aed349991bf8434505eb79d35e17ce
Alley Harmon <112970302+GOODSTROKE@users.noreply.github.com>
2023-02-26 19:16:00 -0500

    Boilerplate removed
    Boilerplate removed

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,5 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+import FilmBreaks from './FilmBreaks'
+import Table from './Table'
+import React, { Component } from 'react'
+
+/* At the top of the App.js file, import useState from react
+import { useState } from "react";
+Add a new state variable to called "title" along with its setter function "setTitle" initalized to an empty string:
+const [title, setTitle] = useState("")
+In the JSX of (inside the enclosing divs), add a new text input field and a label that says "Title:"
+return (
+*/
+
+
+
+
 
 function App() {
   return (
